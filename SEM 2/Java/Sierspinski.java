f8133c8f16c831bf637c40dec26d8b11459684dc
xarcode <ayush.idnan@gmail.com>
2023-01-16 11:04:37 +0530

    sierpinski done

--- SEM 2/Java/Sierspinski.java
+++ SEM 2/Java/Sierspinski.java
@@ -1,32 +1,12 @@
 class Sierpinski
 {
-	static void printSierpinski1(int n)
-	{
-		for (int y = n - 1; y >= 0; y--) {
-
-			for (int i = 0; i < y; i++) {
-				System.out.print(" ");
-			}
-
-			for (int x = 0; x + y < n; x++) {
-				if ((x & y) != 0)
-					System.out.print(" "
-									+ " ");
-				else
-					System.out.print("* ");
-			}
-
-			System.out.print("\n");
-		}
-	}
-
     static void printSierpinski(int n){
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i == 0 || (i & j) != 0)
-                    System.out.print("*");
+                if((i & j) == i)
+                    System.out.print("* ");
                 else 
-                    System.out.print(" ");
+                    System.out.print("  ");
             }
             System.out.println();
         }
@@ -34,7 +14,7 @@ class Sierpinski
 
 	public static void main(String args[])
 	{
-		int n = 4;
+		int n = 32;
 		printSierpinski(n);
 	}
 }
